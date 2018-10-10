dd4163bde9c8c0df0dcf0a68d9e9277cc2400a3e
Lee Campbell <campbel2@tcd.ie>
2018-10-10 11:37:34 +0100

    incr and legal fucntions complete

---
+++ Assignment 1/db.pl
@@ -0,0 +1,14 @@
+pterm(null).
+pterm(f0(X)) :- pterm(X).
+pterm(f1(X)) :- pterm(X).
+
+same(null, null).
+same(f0(X), f0(Y)) :- same(X, Y).
+same(f1(X), f1(Y)) :- same(X, Y).
+
+incr(null, f1(null)).
+incr(f0(X), f1(Y)) :- same(X, Y).
+incr(f1(X), f0(Y)) :- incr(X, Y).
+
+legal(f0(null)).
+legal(Z) :- legal(X), incr(X, Z).
