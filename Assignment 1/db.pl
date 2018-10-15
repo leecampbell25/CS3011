80693f3da4b6cd2e7034cdc8ecbead54b1ceab7d
Lee Campbell <campbel2@tcd.ie>
2018-10-15 20:07:00 +0100

    finished all 6 parts

--- Assignment 1/db.pl
+++ Assignment 1/db.pl
@@ -21,3 +21,15 @@ add(X, Y, Z) :- incr(A, X), add(A, Y, B), incr(B, Z).
 mult(f1(null), Y, Y).
 mult(X, Y, Z) :- incr(A, X), mul(A, Y, B), add(B, Z).
 %----------------------------------------------------------%
+revers(X, Y) :- revers1(X, null, Y).
+
+revers1(null, X, X).
+revers1(f0(X), A, Y) :- revers1(X, f0(A), Y).
+revers1(f1(X), A, Y) :- revers1(X, f1(A), Y).
+%----------------------------------------------------------%
+normalize(null, f0(null)).
+normalize(f0(null), f0(null)).
+normalize(X, Y) :- revers(X, Z), trim(Z, A), revers(A, Y).
+
+trim(f1(X), f1(X)).
+trim(f0(X), A) :- trim(X, A).
